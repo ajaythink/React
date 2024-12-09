3fb97609403079c34adbaf155e5251aa7936ab07
Ajay Kumar <ajaykumar286458@gmail.com>
2024-12-09 10:02:56 +0530

    github auto file

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1>
+        React with Chai 
+      </h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08miniContext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// Create a context object with a default value of an empty object {} and assign it to the UserContext variable  using the React.createContext() method.   
+
+const UserContext = React.createContext();
+
+export default UserContext;
